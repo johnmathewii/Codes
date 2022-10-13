ba50e7ad92e25129b475ced473e9cb6283bd5f30
johnmathewii <prnmathewii@gmail.com>
2022-10-13 11:47:13 +0530

    completed writing the recursive hcf part.

--- RecurseFunc.c
+++ RecurseFunc.c
@@ -2,5 +2,24 @@
 
 int HCF(int a, int b)
 {
-    
+    int rem = a%b;
+    if(rem == 0)
+    {
+        return b;
+    }
+    else
+    {
+        HCF(b,rem);
+    }
+}
+
+
+
+int main()
+{
+    int hcf = HCF(360,198);
+    printf("HCF is %d", hcf);
+   
+
+    return 0;
 }
