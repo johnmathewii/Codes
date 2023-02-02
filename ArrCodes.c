01449bb167533f2b86280c4b6e7a4d7e67a5fa49
johnmathewii <prnmathewii@gmail.com>
2023-02-02 12:15:11 +0530

    few to no modification as last one

--- ArrCodes.c
+++ ArrCodes.c
@@ -1,95 +1,93 @@
-#include<stdio.h>
+#include <stdio.h>
 
-void func1(int arr[], size_t n) //To find the largest element from the array.
+void func1(int arr[], size_t n) // To find the largest element from the array.
 {
     int i, largest;
     largest = arr[0];
-    for(i = 1; i < n; ++i)
-    { 
-        if(largest < arr[i])
+    for (i = 1; i < n; ++i)
+    {
+        if (largest < arr[i])
             largest = arr[i];
     }
     printf("Largest element = %d\n", largest);
     printf("%d\n", n);
 }
 
-void func2(int arr[], int n) //To print the reverse of the  given array.
+void func2(int arr[], int n) // To print the reverse of the  given array.
 {
-    int arr2[n], j=0;
-    for (int i = n-1; i >= 0; i--)        
-    {   
-        arr2[j]=arr[i];
+    int arr2[n], j = 0;
+    for (int i = n - 1; i >= 0; i--)
+    {
+        arr2[j] = arr[i];
         j++;
     }
     for (int i = 0; i < n; i++)
     {
-        printf("%d\n",arr2[i]);
-    }   
+        printf("%d\n", arr2[i]);
+    }
 }
 
-void func3(int arr1[], int arr2[], int n1, int n2) //Merging two arrays into one array.
+void func3(int arr1[], int arr2[], int n1, int n2) // Merging two arrays into one array.
 {
-    int size = n1 + n2; 
+    int size = n1 + n2;
     int arr3[size];
     for (int i = 0; i < n1; i++)
     {
-        arr3[i]=arr1[i];
+        arr3[i] = arr1[i];
     }
-    for (int j = arr3[n1],i = 0; i < n2, j < size; i++, j++)
+    for (int j = arr3[n1], i = 0; i < n2, j < size; i++, j++)
     {
-        arr3[j]=arr2[i];
+        arr3[j] = arr2[i];
     }
     for (int k = 0; k < size; k++)
     {
-        printf("%d\n",arr3[k]);
+        printf("%d\n", arr3[k]);
     }
 }
 
-void func4(int arr[], int size) //To delete duplicate elements from an array. 
+void func4(int arr[], int size) // To delete duplicate elements from an array.
 {
     for (int i = 0; i < size; i++)
     {
-        for (int j = i+1; j < size;)
+        for (int j = i + 1; j < size;)
         {
-            if (arr[i]==arr[j])
+            if (arr[i] == arr[j])
             {
                 for (int k = j; k < size; k++)
                 {
-                    arr[k] = arr[k+1];
+                    arr[k] = arr[k + 1];
                 }
                 size--;
             }
             else
-                j++;    
+                j++;
         }
     }
     for (int i = 0; i < size; i++)
     {
-        printf("%d \n",arr[i]);
+        printf("%d \n", arr[i]);
     }
-    
 }
 
-void func5(int arr1[][4], int r , int c )//To find sum of row elements of given matrix.
+void func5(int arr1[][4], int r, int c) // To find sum of row elements of given matrix.
 {
-    int arr2[]={0,0,0,0};
+    int arr2[] = {0, 0, 0, 0};
 
     for (int i = 0; i < r; i++)
     {
         for (int j = 0; j < c; j++)
         {
             arr2[i] += arr1[i][j];
-        }  
+        }
     }
     for (int i = 0; i < r; i++)
     {
-        printf("%d\n",arr2[i]);
+        printf("%d\n", arr2[i]);
     }
-  
 }
-void func6(int arr1[3][4], int arr2[3][4], int r, int c) //Subtraction of two matrices.
+void func6(int arr1[3][4], int arr2[3][4], int r, int c) // Subtraction of two matrices.
 {
-    int arr3[3][4] = {0,0,0,0,0,0,0,0,0,0,0,0};
+    int arr3[3][4] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
     for (int i = 0; i < r; i++)
     {
         for (int j = 0; j < c; j++)
@@ -101,47 +99,43 @@ void func6(int arr1[3][4], int arr2[3][4], int r, int c) //Subtraction of two ma
     {
         for (int j = 0; j < c; j++)
         {
-            printf("%d,\t",arr3[i][j]);
+            printf("%d,\t", arr3[i][j]);
         }
         printf("\n");
     }
-
 }
 
-void func7(int arr[][4], int r, int c) //Lower Traingle of a sqaure matrix
+void func7(int arr[][4], int r, int c) // Lower Traingle of a sqaure matrix
 {
     for (int i = 0; i < r; i++)
     {
         for (int j = 0; j < c; j++)
         {
-            if(i<j)
+            if (i < j)
             {
                 arr[i][j] = 0;
-            }   
+            }
         }
-        
     }
     for (int i = 0; i < r; i++)
     {
         for (int j = 0; j < c; j++)
         {
-            printf("%d,\t",arr[i][j]);
+            printf("%d,\t", arr[i][j]);
         }
         printf("\n");
     }
-    
-    
 }
 
 void func8(char str[])
 {
     char substr[50][50] = {0};
-    int k=0, i=0, j=0, len, a, max, min, maxIndex = 0, minIndex = 0;
+    int k = 0, i = 0, j = 0, len, a, max, min, maxIndex = 0, minIndex = 0;
 
     while (str[k] != '\0')
     {
-        j=0;
-        while(str[k] != ' ' && str[k] != '\0' && str[k] != '.')   
+        j = 0;
+        while (str[k] != ' ' && str[k] != '\0' && str[k] != '.')
         {
             substr[i][j] = str[k];
             j++;
@@ -152,7 +146,7 @@ void func8(char str[])
         if (str[k] != '\0')
         {
             k++;
-        }   
+        }
     }
     len = i;
     max = strlen(substr[0]);
@@ -161,37 +155,35 @@ void func8(char str[])
     for (int m = 0; m < len; m++)
     {
         a = strlen(substr[m]);
-        if(a>max)
+        if (a > max)
         {
             max = a;
             maxIndex = m;
         }
-        if (a<min)
+        if (a < min)
         {
             min = a;
             minIndex = m;
         }
     }
 
-    printf("Largest Word = %s , Smallest Word = %s \n",substr[maxIndex], substr[minIndex]);
+    printf("Largest Word = %s , Smallest Word = %s \n", substr[maxIndex], substr[minIndex]);
 }
 
-
 int main()
-{   
-    int arr1[][4] = {1,1,1,1,2,2,2,2,3,3,3,3,4,4,4,4};
-    int arr2[][4] = {2,9,3,1,2,5,1,2,2,3,4,1};
+{
+    int arr1[][4] = {1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4};
+    int arr2[][4] = {2, 9, 3, 1, 2, 5, 1, 2, 2, 3, 4, 1};
 
     char str[50] = {"Brownie is a dog"};
-    //int n1 = sizeof(arr1)/sizeof(arr1[0]); //to find no. of elements in an array.
-    //int n2 = sizeof(arr2)/sizeof(arr2[0]); 
-    
-    
-    //func3(arr1,arr2,n1,n2);
-    //func4(arr1,n1);
-    //func5(arr3,3,4);
-    //func6(arr1,arr2,3,4);
-    //func7(arr1,4,4);
+    // int n1 = sizeof(arr1)/sizeof(arr1[0]); //to find no. of elements in an array.
+    // int n2 = sizeof(arr2)/sizeof(arr2[0]);
+
+    // func3(arr1,arr2,n1,n2);
+    // func4(arr1,n1);
+    // func5(arr3,3,4);
+    // func6(arr1,arr2,3,4);
+    // func7(arr1,4,4);
     func8(str);
     return 0;
 }
--- Searcodes.c
+++ Searcodes.c
@@ -1,31 +1,31 @@
-#include<stdio.h>
+#include <stdio.h>
 
-void func1(int arr[],int size, int t)
+void func1(int arr[], int size, int t) 
 {
     int count = 0;
     for (int i = 0; i < size; i++)
     {
-        if(t == arr[i])
+        if (t == arr[i])
         {
             count++;
-            printf("%d is present at loaction %d\n",t,i);
+            printf("%d is present at loaction %d\n", t, i);
         }
     }
     if (count == 0)
         printf("The no is not present in the list.");
     else
-        printf("%d is present %d times\n",t, count);
+        printf("%d is present %d times\n", t, count);
 }
 
-int Binary_S(int arr[],int index, int size, int target)
+int Binary_S(int arr[], int index, int size, int target) //Binary Search Function
 {
-    int m = (index+size)/2;
-    if(index <= size)
+    int m = (index + size) / 2;
+    if (index <= size)
     {
         if (target > arr[m])
         {
             index = m + 1;
-            Binary_S(arr, index, size, target); 
+            Binary_S(arr, index, size, target);
         }
         else if (target < arr[m])
         {
@@ -33,47 +33,88 @@ int Binary_S(int arr[],int index, int size, int target)
             Binary_S(arr, index, size, target);
         }
         else if (target == arr[m])
-            return m;  
+            return m;
     }
     else
         return -1;
- 
 }
 
-int func2(int arr[],int index,int size)
+int func2(int arr[], int index, int size)
 {
     int temp;
-    if(index < size)
+    if (index < size)
     {
         temp = arr[index];
         arr[index] = arr[size];
         arr[size] = temp;
-        index++; size--;
-        func2(arr,index,size);
+        index++;
+        size--;
+        func2(arr, index, size);
     }
     else
     {
-        for (int i = 0; i <= size+index; i++)
+        for (int i = 0; i <= size + index; i++)
         {
-            printf("%d ",arr[i]);
+            printf("%d ", arr[i]);
         }
+    }
+}
+
+void func3(int arr1[], int arr2[], int n1, int n2)
+{
+    int i = 0, j = 0, k = 0;
+    int size = n1 + n2;
+    int arr3[size];
+
+    while (i < n1 && j < n2)
+    {
+        if (arr1[i] < arr2[j])
+        {
+            arr3[k] = arr1[i];
+            i++;
+            k++;
+        }
+        else
+        {
+            arr3[k] = arr2[j];
+            j++;
+            k++;
+        }
+    }
+
+    while (i < n1)
+    {
+        arr3[k++] = arr1[i++];
+    }
+
+    while (j < n2)
+    {
+        arr3[k++] = arr2[j++];
+    }
+
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ", arr3[i]);
     }
 }
 
 int main()
 {
-    //int arr[] = {7,30,50,90,30,70,30,30,30};
-    int arr[] = {8,9,10,6,4,7,11};
-    int size = sizeof(arr)/sizeof(arr[0]);
-    //func1(arr,size,30);
-    
-    func2(arr,0,size-1);
-    // int target = 7;
-    // int found = Binary_S(arr,0,size,target);
-    // if (found == -1)
-    //     printf("The no %d is not in the list.",target);
-    // else
-    //     printf("The no. %d is in the location %d",target,found);
-    // return 0; 
+    int arr1[] = {20, 30, 40, 60, 70, 80, 90};
+    int arr2[] = {10, 50, 100};
+    int size1 = sizeof(arr1) / sizeof(arr1[0]);
+    int size2 = sizeof(arr2) / sizeof(arr2[0]);
 
+    func3(arr1, arr2, size1, size2);
+
+    // func1(arr,size,30);
+
+    // func2(arr,0,size-1);
+    //  int target = 7;
+    //  int found = Binary_S(arr,0,size,target);
+    //  if (found == -1)
+    //      printf("The no %d is not in the list.",target);
+    //  else
+    //      printf("The no. %d is in the location %d",target,found);
+    //  return 0;
 }
